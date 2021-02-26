1c80bba8f06122c6aa2fa02d76cf9a8a4ca1ad4b
Vuorikko Salla <s2100129@edu.bc.fi>
2021-02-26 19:35:19 +0200

    portfolio

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,15 +1,24 @@
 let nav = document.querySelector("nav");
 let ul = document.querySelector("navUl");
+let buttonBackToTop = document.getElementById("backToTop");
 
-const scrollNav = () => {
+const scrollFunction = () => {
     if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
         nav.style.background = "#134038b3";
         navUl.style.padding = "1%";
+        buttonBackToTop.style.display = "block";
     } else {
         nav.style.background = "transparent";
         navUl.style.padding = "3.7%";
-        
+        buttonBackToTop.style.display = "none";
     }
 };
 
-window.addEventListener("scroll", scrollNav);
+    const getToTop = () => {
+        document.body.scrollTop = 0;
+        document.documentElement.scrollTop = 0;
+      }
+
+
+window.addEventListener("scroll", scrollFunction);
+buttonBackToTop.addEventListener("click", getToTop);
