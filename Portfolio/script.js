fe82cdbd446f447a46f31261a8ebf3099aaa0cf0
Vuorikko Salla <s2100129@edu.bc.fi>
2021-03-21 21:12:42 +0200

    portfolio mobile support

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,22 +1,37 @@
 let nav = document.querySelector("nav");
 let ul = document.querySelector("navUl");
 let buttonBackToTop = document.getElementById("backToTop");
+let buttonMobile = document.getElementById("mobileButton");
+let links = document.querySelectorAll("nav ul li a");
 
 const scrollFunction = () => {
-    if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
-        nav.classList.add("bg");
-        buttonBackToTop.style.display = "block";
-    } else {
-        nav.classList.remove("bg");
-        buttonBackToTop.style.display = "none";
-    }
+  if (document.body.scrollTop > 90 || document.documentElement.scrollTop > 90) {
+    nav.classList.add("bg");
+    buttonBackToTop.style.display = "block";
+  } else {
+    nav.classList.remove("bg");
+    buttonBackToTop.style.display = "none";
+  }
 };
 
-    const getToTop = () => {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-      }
+const getToTop = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+};
 
+const mobMenu = () => {
+  for (let i = 0; i < links.length; i++) {
+    links[i].addEventListener("click", mobMenu);
+  }
+  if (nav.classList.contains("responsive")) {
+    nav.classList.remove("responsive");
+    document.body.style.overflow = "";
+  } else {
+    nav.classList.add("responsive");
+    document.body.style.overflow = "hidden";
+  }
+};
 
 window.addEventListener("scroll", scrollFunction);
 buttonBackToTop.addEventListener("click", getToTop);
+buttonMobile.addEventListener("click", mobMenu);
